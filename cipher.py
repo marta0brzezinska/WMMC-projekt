8f44570cb9b114e818f29a64c3e6bc525c7198e0
Brzezinska_Marta_Ida <01124845@pw.edu.pl>
2022-03-29 10:41:15 +0200

    szablon projektu

---
+++ cipher.py
@@ -0,0 +1,15 @@
+""""
+Implementacja szyfru blokowego zaprojektownego w ramach projektu z przedmiotu WMMC.
+
+Marta Brzezińska
+Kinga Kawczyńska
+"""
+
+def encrypt(plaintext, key):
+    return None
+
+def decrypt(ciphertext, key):
+    return None
+
+def key_schedule(key):
+    return None
