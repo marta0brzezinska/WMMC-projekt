f3d165167c61507786ca76b8ccbb37d788b0aa34
Kinga Kawczyńska <kinga.7.kaw@gmail.com>
2022-03-31 16:31:55 +0200

    dzialający kod na intach

--- type_transformations.py
+++ type_transformations.py
@@ -1,31 +1,41 @@
-def int_to_list(input):
-    #TODO: poprawić kod (bytes->list)
-    input = input
-
-    l = len(input) * 8
-    result = [0] * l
-    pos = 0
-    for ch in input:
-        i = 7
-        while i >= 0:
-            if ch & (1 << i) != 0:
-                result[pos] = 1
-            else:
-                result[pos] = 0
-            pos += 1
-            i -= 1
+def int_to_list(inp):
+    ''' convert int to list of bits '''
+    result = [0]*64
+    i = 63
+    while inp > 0:
+        result[i] = inp % 2
+        inp = inp >> 1
+        i = i-1
     return result
 
 
-def list_to_int(input):
-    #TODO: poprawić kod (list->bytes)
-    result = []
-    pos = 0
-    c = 0
-    while pos < len(input):
-        c += input[pos] << (7 - (pos % 8))
-        if (pos % 8) == 7:
-            result.append(c)
-            c = 0
-        pos += 1
-    return bytes(result)
+def list_to_int(inp):
+    ''' convert list of bits to int '''
+    result = 0
+    for i in range(64):
+        result = (result << 1) + inp[i]
+    return result
+
+
+def int_to_blocks(inp):
+    ''' convert int to list of 8-bit blocks '''
+    blocks = [0]*8
+    i = 7
+    while inp > 0:
+        temp = inp >> 8
+        blocks[i] = inp - (temp << 8)
+        inp = inp >> 8
+        i = i-1
+    return blocks
+
+
+def blocks_to_int(inp):
+    ''' convert list of 8-bit blocks to int '''
+    result = 0
+    for i in inp:
+        result += i
+        result = result << 8
+    result = result >> 8
+    return result
+    
+    
