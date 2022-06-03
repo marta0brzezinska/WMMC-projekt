72ee5dc2c2494157214081c9eedcd18cf1ea2b92
Brzezinska_Marta_Ida <01124845@pw.edu.pl>
2022-06-03 19:43:06 +0200

    update zadanie3 + zadanie4

--- projekt2/zadania.py
+++ projekt2/zadania.py
@@ -10,11 +10,29 @@ rozw1 = ""
 tresc2 = ""
 rozw2 = ""
 
-tresc3 = "Czy kryptosystemy McEliece’a i Niederreiter’a są sobie równoważne? Udowodnij."
+tresc3 = "Rozważmy kryptosystem McEliece’a, gdzie wykorzystując klucz publiczny G′ szyfrujemy wiadomość za pomocą wzoru: \n" \
+         "c = mG′ + e \n" \
+         "oraz kryptosystem Niederreiter’a, w którym wykorzystując klucz publiczny H′ szyfrujemy za pomocą wzoru:\n" \
+         "s = eH′T \n" \
+         "Czy bezpieczeństwa tych kryptosystemów są sobie równoważne? Udowodnij."
 rozw3 = "tak"
 
-tresc4 = ""
-rozw4 = ""
+tresc4 = "Rozważmy kod splotowy o r=2 i K=3. Niech: \n" \
+         "g1(x1,x2,x3)=x3+x2, \n" \
+         "g2(x1,x2,x3)=x3+x2+x1, \n" \
+         "p=(0,0).\n" \
+         "\n" \
+         "Za jego pomocą zostało zakodowanych 8 6-bitowych słów i otrzymano następujące wyniki:\n" \
+         "(1,1,1,1,0,1,1,1,1,1,0,1)\n" \
+         "(1,0,1,1,0,1,1,1,0,0,0,1)\n" \
+         "(0,0,1,1,0,0,1,0,1,0,0,0)\n" \
+         "(1,1,1,1,0,1,0,0,1,1,0,0)\n" \
+         "(0,1,0,0,1,1,1,1,1,1,0,0)\n" \
+         "(0,0,1,1,1,1,0,1,1,1,0,0)\n" \
+         "(1,1,0,0,1,0,1,0,1,1,0,1)\n" \
+         "(1,1,0,0,1,0,1,1,1,1,0,1)\n" \
+         "W ilu przypadkach został popełniony błąd podczas kodowania?"
+rozw4 = "3"
 
 def zadania():
     print("Start!")
@@ -26,7 +44,7 @@ def zadania():
         print("Niepoprawne rozwiązanie, spróbuj jeszcze raz!")
         odp1 = input()
 
-    print("Super! Przechodzimy do kolejnego zadania.")
+    print("Super! Przechodzimy do kolejneg zadania.")
     print("Zadanie 2")
     print(tresc2)
     odp2 = input()
