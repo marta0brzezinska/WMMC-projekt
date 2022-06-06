059ce7da037847008be7813ea86271691fb7a8d3
Kinga Kawczyńska <kinga.7.kaw@gmail.com>
2022-06-06 21:45:34 +0200

    dokonczenie -  zad3 i 4

--- projekt2/zadania.py
+++ projekt2/zadania.py
@@ -4,11 +4,61 @@ Implementacja programu w ramach Mikro-Projektu “CodesMicroHack” z przeddmiot
 Marta Brzezińska
 Kinga Kawczyńska
 """
-tresc1 = ""
-rozw1 = ""
+tresc1 = "Czy dla dowolnej macierzy generującej kod liniowy zawsze można jednoznacznie \n"\
+        "wyznaczyć macierz kontroli parzystości z dokładnością do zmiany kolejności wierszy. "
+rozw1 = "nie"
 
-tresc2 = ""
-rozw2 = ""
+tresc2 = "Rozważmy automat skończony o 64 stanach dla kodu splotowego o parametrach r = 4 i K = 7, \n" \
+            "którego wartości przejść (wartości na krawędziach diagramu) znajdują się w liscie automat. \n"  \
+            "Listę tę czytamy tak, że na $i$-tym indeksie (indeksujemy od zera) znajduje się zakodowana \n" \
+            "(naszym kodem) binarna reprezentacja liczby i. Pierwsza cyfra binarnej reprezentacji i determinuje przejście do następnego stanu, a pozostałe stan wychodzący. Wiemy, że jedna z wartości przejsc \n" \
+            "w automacie jest błędna. Znajdź ją i popraw (wartość ta jest flagą)."
+automat = [[0, 0, 0, 0], [1, 0, 1, 1], [0, 0, 1, 1], 
+           [1, 0, 0, 0], [1, 1, 0, 1], [0, 1, 1, 0], 
+           [1, 1, 1, 0], [0, 1, 0, 1], [1, 1, 0, 0], 
+           [0, 1, 1, 1], [1, 1, 1, 1], [0, 1, 0, 0], 
+           [0, 0, 0, 1], [1, 0, 1, 0], [0, 0, 1, 0], 
+           [1, 0, 0, 1], [0, 0, 1, 0], [1, 0, 0, 1], 
+           [0, 0, 0, 1], [1, 0, 1, 0], [1, 1, 1, 1], 
+           [0, 1, 0, 0], [1, 0, 0, 1], [0, 1, 1, 1],
+           [1, 1, 1, 0], [0, 1, 0, 1], [1, 1, 0, 1], 
+           [0, 1, 1, 0], [0, 0, 1, 1], [1, 0, 0, 0], 
+           [0, 0, 0, 0], [1, 0, 1, 1], [0, 1, 1, 0], 
+           [1, 1, 0, 1], [0, 1, 0, 1], [1, 1, 1, 0], 
+           [1, 0, 1, 1], [0, 0, 0, 0], [1, 0, 0, 0], 
+           [0, 0, 1, 1], [1, 0, 1, 0], [0, 0, 0, 1], 
+           [1, 0, 0, 1], [0, 0, 1, 0], [0, 1, 1, 1], 
+           [1, 1, 0, 0], [0, 1, 0, 0], [1, 1, 1, 1], 
+           [0, 1, 0, 0], [1, 1, 1, 1], [0, 1, 1, 1], 
+           [1, 1, 0, 0], [1, 0, 0, 1], [0, 0, 1, 0], 
+           [1, 0, 1, 0], [0, 0, 0, 1], [1, 0, 0, 0], 
+           [0, 0, 1, 1], [1, 0, 1, 1], [0, 0, 0, 0], 
+           [0, 1, 0, 1], [1, 1, 1, 0], [0, 1, 1, 0], 
+           [1, 1, 0, 1], [1, 0, 0, 1], [0, 0, 1, 0], 
+           [1, 0, 1, 0], [0, 0, 0, 1], [0, 1, 0, 0], 
+           [1, 1, 1, 1], [0, 1, 1, 1], [1, 1, 0, 0], 
+           [0, 1, 0, 1], [1, 1, 1, 0], [0, 1, 1, 0], 
+           [1, 1, 0, 1], [1, 0, 0, 0], [0, 0, 1, 1], 
+           [1, 0, 1, 1], [0, 0, 0, 0], [1, 0, 1, 1], 
+           [0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 1, 1], 
+           [0, 1, 1, 0], [1, 1, 0, 1], [0, 1, 0, 1], 
+           [1, 1, 1, 0], [0, 1, 1, 1], [1, 1, 0, 0], 
+           [0, 1, 0, 0], [1, 1, 1, 1], [1, 0, 1, 0], 
+           [0, 0, 0, 1], [1, 0, 0, 1], [0, 0, 1, 0], 
+           [1, 1, 1, 1], [0, 1, 0, 0], [1, 1, 0, 0], 
+           [0, 1, 1, 1], [0, 0, 1, 0], [1, 0, 0, 1], 
+           [0, 0, 0, 1], [1, 0, 1, 0], [0, 0, 1, 1], 
+           [1, 0, 0, 0], [0, 0, 0, 0], [1, 0, 1, 1], 
+           [1, 1, 1, 0], [0, 1, 0, 1], [1, 1, 0, 1], 
+           [0, 1, 1, 0], [1, 1, 0, 1], [0, 1, 1, 0], 
+           [1, 1, 1, 0], [0, 1, 0, 1], [0, 0, 0, 0], 
+           [1, 0, 1, 1], [0, 0, 1, 1], [1, 0, 0, 0], 
+           [0, 0, 0, 1], [1, 0, 1, 0], [0, 0, 1, 0], 
+           [1, 0, 0, 1], [1, 1, 0, 0], [0, 1, 1, 1], 
+           [1, 1, 1, 1], [0, 1, 0, 0]]
+
+
+rozw2 = "[1, 1, 0, 0]"
 
 tresc3 = "Rozważmy kryptosystem McEliece’a, gdzie wykorzystując klucz publiczny G′ szyfrujemy wiadomość za pomocą wzoru: \n" \
          "c = mG′ + e \n" \
@@ -47,6 +97,7 @@ def zadania():
     print("Super! Przechodzimy do kolejneg zadania.")
     print("Zadanie 2")
     print(tresc2)
+    print('automat = ', automat)
     odp2 = input()
 
     while odp2 != rozw2:
